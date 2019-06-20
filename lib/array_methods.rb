046520158ee58d980c14378dd05b7929d22fab30
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-20 12:02:51 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ def find_element_index(array, value_to_find)
+  array.index(value_to_find)
 end
 
+find_element_index([1,3,4,5,6,2,9,10],3)
+
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
+
